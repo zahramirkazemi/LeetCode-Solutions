a3b926a28b1b4cf3b3814baca7104a388aebccf1
zahramirkazemi <zahramirkazemi1997@gmail.com>
2022-05-09 15:33:54 +0430

    add two solution

---
+++ palindrome-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+ var isPalindrome = function(x) {
+    if(x === Number(String(x).split('').reverse().join('')))
+    {
+        return true;
+    }
+    else{
+        return false;    
+    }
+};
