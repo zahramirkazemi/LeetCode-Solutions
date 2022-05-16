897320c7f6d3f4b24749f152c7a0e340d673bb38
zahramirkazemi <zahramirkazemi1997@gmail.com>
2022-05-16 19:35:35 +0430

    add generator row

--- addRow.js
+++ addRow.js
@@ -7,11 +7,10 @@ function addRow(event){
     let lang = document.getElementById("Lang").value;
     let difficulty = document.getElementById("Difficulty").value;
     let data = { id: Id, title: title, lang: lang, Difficulty: difficulty};
-    postData("https://my-json-server.typicode.com/zahramirkazemi/Leetcode-Solutions/Rows",data);
+    postData("http://localhost:3000/Rows",data);
     event.preventDefault();
 }
 async function postData(url = '', data = {}) {
-    console.log(JSON.stringify(data))
     const response = await fetch(url, {
       method: 'POST',
       headers: {
