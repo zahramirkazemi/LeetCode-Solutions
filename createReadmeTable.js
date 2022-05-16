c4760b1be6dc8b376cffa97ca70e2826edeae8bc
zahramirkazemi <zahramirkazemi1997@gmail.com>
2022-05-16 19:37:41 +0430

    update generator row

--- createReadmeTable.js
+++ createReadmeTable.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 let jsonData = require('./db.json');
 data = `# LeetCode-Solutions \nmy solutions to leetcode.com problems in javascript language.\ntotal: ${jsonData.Rows.length} \n \n| ID | Title | Solution | Difficulty |\n|---| ----- | -------- | ---------- |`
 for(r of jsonData.Rows.sort((a, b) => (parseInt(a.id) - parseInt(b.id)) )){
-    data += `\n|${r.id}|[${r.title}](https://leetcode.com/problems/${r.title}/) | [${r.lang}](${r.title}.js)|${r.Difficulty}|\n`
+    data += `\n|${r.id}|[${r.title}](https://leetcode.com/problems/${r.title}/) | [${r.lang}](${r.title}.js)|${r.Difficulty}|`
 }
 fs.writeFile('README.md', data, (err) => {
      if (err) throw err;
